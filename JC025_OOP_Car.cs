e17375e75e0e0e7d4f944280977a01879e159d57
yufeng liu <liuyufeng_hnzz@163.com>
2026-02-28 09:39:38 +0800

    新增面向对象基础知识添加项目文件。

---
+++ JC025_OOP_Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstStep
+{
+    /*
+    5.	创建一个 Car 类
+    具体步骤：
+    1.	定义类 Car，包含属性 Brand、Model、Year
+    2.	添加 StartEngine() 方法
+    */
+    internal class JC025_OOP_Car
+    {
+        public string Brand {  get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void StartEngine()
+        {
+            Console.WriteLine($"{Brand}{Model} is starting");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace FirstStep
             //JC007_NineNineTable.NineNine();
             //JC008_SumEvenInOneHundred.EvenSum();
             //分类？？
-            JC009_PrintDiamondPattern.PrintDiamond();
+            //JC009_PrintDiamondPattern.PrintDiamond();
             //JC010_LoginSystem.Login();
             //JC011_ReturnMaxAndMin.MaxAndMin();
             //交换？？
@@ -39,14 +39,66 @@ namespace FirstStep
             //JC017.CallReverseString();
             //JC018
             //JC018_VerifyEmailFormat JC018 = new JC018_VerifyEmailFormat();
-            //Console.WriteLine(JC018.VerifyEmailFormat());
+            //string verifyEmailFormatResult = JC018.VerifyEmailFormat();
+            //Console.WriteLine(verifyEmailFormatResult);
             //JC019
             //JC019_HandleException JC019 = new JC019_HandleException();
             //JC019.Exception();
             //JC020
             //JC020_SimpleCalculator JC020 = new JC020_SimpleCalculator();
             //JC020.Calculator();
+            //JC021
+            //JC021_OOP_Person JC021 = new JC021_OOP_Person();
+            //JC021.Name= "张三";   
+            //JC021.Age= 10;
+            //JC021.Gender = '男';
+            //JC021.PrintDetails();
+            //JC022
+            //JC022_OOP_Rectangle JC022 = new JC022_OOP_Rectangle();
+            //JC022.Length = 0.8;
+            //JC022.Width = 0.09;
+            //double result = JC022.CalculaterArea();
+            //Console.WriteLine(result);
+            //JC023
+            //JC023_OOP_Student  JC023=new JC023_OOP_Student();
+            //JC023.Name = "李四";
+            //JC023.Grade = "三";
+            //JC023.study();
+            //JC024
+            //JC024_OOP_BankAccount JC024 = new JC024_OOP_BankAccount();
+            //JC024.Balance = 10;
+            //JC024.Amount = 500;
+            //JC024.Deposit();
+            //JC024.Withdraw();
+            //JC025
+            //JC025_OOP_Car JC025=new JC025_OOP_Car();
+            //JC025.Brand = "幻影";
+            //JC025.Model ="888";
+            //JC025.Year = 2026;
+            //JC025.StartEngine();
+            //JC026
+            //多态展示
+            JC026_OOP_Animal_Cat JC026Cat = new JC026_OOP_Animal_Cat("小猫", 1, true );
+ 
+            JC026_OOP_Animal_Dog JC026Dog = new JC026_OOP_Animal_Dog("小狗",2,"金毛");
+           
+            JC026_OOP_Animal_Duck JC026Duck = new JC026_OOP_Animal_Duck("小鸭", 3,true);
 
+            JC026_OOP_Animal[] animals = { JC026Cat, JC026Dog, JC026Duck };
+            foreach (JC026_OOP_Animal animal in animals)
+            {
+                animal.Move();
+                animal.MakeSound();
+            }
+            Console.WriteLine("----------------分割线--------------------");
+            //泛型类
+            var dogShelter = new AnimalShelter<JC026_OOP_Animal_Dog>();
+            dogShelter.AddAnimal(new JC026_OOP_Animal_Dog("大狗", 6, "布迪"));
+            dogShelter.AddAnimal(new JC026_OOP_Animal_Dog("小狗", 7, "泰迪"));
+            dogShelter.DisplayAllAnimals();
+            Console.WriteLine("----------------分割线--------------------");
+            //静态类调用
+            AnimalHelper.PrintAnimalDetails(dogShelter.GetAnimalByName("小狗"));
 
 
         }
